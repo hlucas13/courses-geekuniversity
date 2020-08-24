271b7649b9963e5baf45adf19dd09418e652da5c
Homero do Prado <hmr.prd@gmail.com>
2020-08-24 21:05:29 +0100

    Lessons watched in August 24th

---
+++ javascript/secao04/aula08/script.js
@@ -0,0 +1,39 @@
+// Operadores aritméticos
+
+/*
+adição: +
+subtração: -
+multiplicação: *
+divisão: /
+*/
+
+// Abreviados:
+/*
++=
+-=
+*=
+/=
+*/
+
+console.log(7 / 0); // Infinity
+console.log('42' / 2); // 21
+console.log('Geek' * 3); // NaN (Not a Number)
+console.log(0.1 + 0.7); // 0.79999999999999999...
+
+var num = 42;
+console.log(num.toString());
+console.log((13.343455544).toFixed(3));
+
+console.log(Math.PI);
+console.log(Math.pow(3, 2));
+console.log(Math.sqrt(81));
+
+console.log(typeof 42);
+console.log(typeof 'Geek');
+
+console.log(Number.isInteger(45));
+console.log(Number.isInteger(13.2));
+
+// conversão binária
+var valor = 10;
+console.log(valor.toString(2));
